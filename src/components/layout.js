ea6374ca33598913e7550ec69c59f9c2adfdd677
Mar4elius <marauman@gmail.com>
2020-05-31 15:19:14 -0600

    General style for layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,11 @@ import Header from "../components/header"
 
 export default function Layout({ children }) {
   return (
-    <div>
-      <Header quote="Hello">{children}</Header>
+    <div className="flex bg-main">
       <Sidebar>{children}</Sidebar>
+      <div className="flex flex-wrap flex-col w-full">
+        <Header quote="Hello">{children}</Header>
+      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+    </Layout>
+  )
+}
