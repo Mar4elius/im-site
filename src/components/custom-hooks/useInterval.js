03711a1b048cc5e0f1d92c5acffcb2ecbf56ff3c
Mar4elius <marauman@gmail.com>
2020-07-01 15:19:02 -0600

    Clean ups

--- src/components/custom-hooks/useInterval.js
+++ src/components/custom-hooks/useInterval.js
@@ -1,5 +1,5 @@
 // https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-import React, { useState, useEffect, useRef } from "react"
+import { useEffect, useRef } from "react"
 
 export default function useInterval(callback, delay) {
   const savedCallback = useRef()
