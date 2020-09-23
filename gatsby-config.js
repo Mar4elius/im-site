b3fe8346716ad40df050357fa2de78be3b097697
Mar4elius <marauman@gmail.com>
2020-09-22 20:05:51 -0600

    Install gatsby image packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,12 +15,6 @@ module.exports = {
   },
 
   plugins: [
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-sass`,
-    // service worker plugin
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-remark`,
     {
       //Inclusion of a web app manifest is one of the three generally accepted baseline requirements for a PWA.
       resolve: `gatsby-plugin-manifest`,
@@ -43,5 +37,25 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    //This snippet with resolve, options, name, and path allows me to query the
+    // /img directory inside of /src with GraphQL, which is how I’ll use it with the images in my filesystem.
+    //https://medium.com/@kyle.robert.gill/ridiculously-easy-image-optimization-with-gatsby-js-59d48e15db6e
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`,
+      },
+    },
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-sass`,
+    // service worker plugin
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ],
 }
+
+console.log(__dirname)
