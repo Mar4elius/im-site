6c9def6d1829631021bb1cf25757c39754653c41
Mar4elius <marauman@gmail.com>
2020-09-26 14:31:47 -0600

    Move gatsby analytics to top of config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,17 @@ module.exports = {
 
   plugins: [
     {
+      // docs  https://www.gatsbyjs.com/plugins/gatsby-plugin-google-analytics/
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_KEY,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        anonymize: true,
+      },
+    },
+    {
       //Inclusion of a web app manifest is one of the three generally accepted baseline requirements for a PWA.
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -33,37 +44,6 @@ module.exports = {
         icon: `src/assets/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      // docs  https://www.gatsbyjs.com/plugins/gatsby-plugin-google-analytics/
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-121439989-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "imarchenko.com",
-      },
-    },
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
