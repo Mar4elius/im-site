1f1219ed58bddab93dbcd88ccb032db2147d1d36
Mar4elius <marauman@gmail.com>
2020-09-26 13:23:59 -0600

    Remove empty file

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        cookieDomain: "imarchenko.com",
       },
     },
 
