d751e70d52c08d484854653c955922ada82fe264
Mar4elius <marauman@gmail.com>
2020-09-26 14:22:03 -0600

    Testing solution

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: process.env.GATSBY_GOOGLE_ANALYTICS_KEY,
+        trackingId: "UA-121439989-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
