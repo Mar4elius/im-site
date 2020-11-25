e04c67701fef191a70d20f42cf172104166066f2
Mar4elius <marauman@gmail.com>
2020-11-24 21:09:32 -0700

    Fix display of bottom nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header(props) {
   return (
     <div className="flex justify-center w-full hover-color items-center h-32 text-white">
-      <p className="text-xl sm:text-3xl">{props.quote}</p>
+      <p className="text-xl sm:text-3xl text-center">{props.quote}</p>
     </div>
   )
 }
